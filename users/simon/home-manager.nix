2a7a309b27c6643fecc0494ba9510172119b1c48
Simon Walker <s.r.walker101@googlemail.com>
2022-08-27 18:04:55 +0100

    feat: add nix-direnv
    For direnv flake support

--- users/simon/home-manager.nix
+++ users/simon/home-manager.nix
@@ -87,6 +87,9 @@ let sources = import ../../nix/sources.nix; in {
 
   programs.direnv= {
     enable = true;
+    nix-direnv = {
+        enable = true;
+    };
 
     config = {
       whitelist = {
